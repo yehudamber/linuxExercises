1d342fefac533d3e4163cae974794257449cb0bd
Yehuda <yehudamber@gmail.com>
2021-03-24 12:24:56 +0200

    Add comment on the fmtMode function

--- ch5-catdir/utils.h
+++ ch5-catdir/utils.h
@@ -4,4 +4,9 @@
 
 #include <sys/types.h>
 
+/*
+ * fmtMode - a function that converts a mode_t value to its graphical
+ *           representation (eg. 'drwxr-xr-x')
+ */
+
 std::string fmtMode(mode_t);
