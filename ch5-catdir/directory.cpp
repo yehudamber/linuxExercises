fdc8f6420b5cc7414c06cdc9826bbc3297542ebe
yehudamber <yehudamber@gmail.com>
2021-04-12 22:26:05 +0300

    Chapter 5 Exercise 3
    Handle symbolic links and print their target path

--- ch5-catdir/directory.cpp
+++ ch5-catdir/directory.cpp
@@ -8,6 +8,7 @@
 #include <utility>
 
 #include <sys/stat.h>
+#include <unistd.h>
 
 Directory::Directory(const std::string_view& path)
     : m_path(path), m_dir(::opendir(m_path.c_str()))
@@ -48,14 +49,60 @@ std::optional<Directory::Entry> Directory::next()
                             "failed to read entry from " + m_path);
 }
 
+namespace
+{
+
+/*
+ * readlink - reads the content of the symlink 'path', using initial buffer of
+ *            size 'size'
+ *
+ * We can't rely on the size returned by lstat, because the size of the link can
+ * increase between the calls, and because the kernel doesn't give a size for
+ * the symlinks on pseudo-filesystems such as /proc, so we need to iterate until
+ * the buffer fits the link content.
+ *
+ * The code main layout is based on the GNU coreutils v5.0 xreadlink function.
+ */
+
+std::optional<std::string> readlink(const std::string& path, off_t size)
+{
+    if (size == 0) // the OS doesn't give us the size, so we need to guess
+    {
+        size = 80;
+    }
+    auto res = std::string();
+    while (true)
+    {
+        res.resize(size + 1); // one additional character for the terminating
+                              // null
+        if (auto readCount = ::readlink(path.c_str(), res.data(), res.size());
+            readCount < 0)
+        {
+            auto savedErrno = errno;
+            Controller::error() << "failed to read link " << path << ": "
+                                << std::generic_category().message(savedErrno)
+                                << '\n';
+            return std::nullopt;
+        }
+        else if (readCount <= size)
+        {
+            res.resize(readCount);
+            return res;
+        }
+        size *= 2;
+    }
+}
+
+}
+
 Directory::Entry Directory::entryFor(std::string name)
 {
-    auto res = Entry{ std::move(name), std::nullopt };
+    auto res = Entry{ std::move(name), std::nullopt, std::nullopt };
 
     auto path = m_path + '/' + res.m_name;
 
     struct stat data;
-    if (::stat(path.c_str(), &data) < 0)
+    if (::lstat(path.c_str(), &data) < 0)
     {
         auto savedErrno = errno;
         Controller::error() << "failed to stat " << path << ": "
@@ -65,5 +112,9 @@ Directory::Entry Directory::entryFor(std::string name)
     }
 
     res.m_extra = { data.st_ino, data.st_mode, data.st_nlink };
+    if (S_ISLNK(data.st_mode))
+    {
+        res.m_linkTarget = readlink(path, data.st_size);
+    }
     return res;
 }
